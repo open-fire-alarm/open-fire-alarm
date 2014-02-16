43a1524dff83588aead4d8da21bd37f8755b0525
perecastor <archer.tb@hotmail.fr>
2014-02-16 14:25:46 +0000

    amelioration du display

--- source/Display.c
+++ source/Display.c
@@ -27,6 +27,65 @@ int display()
     {
         printf("zone 3 on fire\n\r");
     }
+
     printf("\n\r");
+
+    if(zones.zone1 == 0)
+    {
+        display_zone1_safe();
+    }
+    else
+    {
+        display_zone1_fire();
+    }
+    if(zones.zone2 == 0)
+    {
+        display_zone2_safe();
+    }
+    else
+    {
+        display_zone2_fire();
+    }
+    if(zones.zone3 == 0)
+    {
+        display_zone3_safe();
+    }
+    else
+    {
+        display_zone3_fire();
+    }
+
+    printf("\n\r");
+
 	return 0;
+}
+
+int display_zone1_safe()
+{
+    return 0;
+}
+
+int display_zone1_fire()
+{
+    return 0;
+}
+
+int display_zone2_safe()
+{
+    return 0;
+}
+
+int display_zone2_fire()
+{
+    return 0;
+}
+
+int display_zone3_safe()
+{
+    return 0;
+}
+
+int display_zone3_fire()
+{
+    return 0;
 }
