9e37599a3ebaf52d8463150fce19d71d984f75fa
perecastor <archer.tb@hotmail.fr>
2014-03-19 02:24:43 +0100

    gestion de si un autre int est taper (autre que 0 et 1) le int est dedemande a l utilisateur

--- source/Shell.c
+++ source/Shell.c
@@ -6,16 +6,25 @@ int activate_zones()
 {
     int commande_utilisateur;
 
-    printf("zone 1 active = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("zone 1 active = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_zones_active1(commande_utilisateur);
 
-    printf("zone 2 active = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("zone 2 active = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_zones_active2(commande_utilisateur);
 
-	printf("zone 3 active = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("zone 3 active = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_zones_active3(commande_utilisateur);
 
 	update_mylog_zones_active();
@@ -27,40 +36,64 @@ int activate_alarms ()
 {
     int commande_utilisateur;
 
-	printf("alarm 1 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 1 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm1(commande_utilisateur);
 
-	printf("alarm 2 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 2 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm2(commande_utilisateur);
 
-	printf("alarm 3 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 3 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm3(commande_utilisateur);
 
-	printf("alarm 4 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 4 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm4(commande_utilisateur);
 
-	printf("alarm 5 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 5 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm5(commande_utilisateur);
 
-	printf("alarm 6 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 6 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm6(commande_utilisateur);
 
-	printf("alarm 7 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 7 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm7(commande_utilisateur);
 
 	printf("alarm 8 = ");
 	scanf("%d", &commande_utilisateur);
 	set_alarm8(commande_utilisateur);
 
-	printf("alarm 9 = ");
-	scanf("%d", &commande_utilisateur);
+    do
+    {
+        printf("alarm 9 = ");
+        scanf("%d", &commande_utilisateur);
+    } while(!(commande_utilisateur==0 || commande_utilisateur==1));
 	set_alarm9(commande_utilisateur);
 
     update_zones(); /* modifie les valeurs des zones automatiquement quand on change les valeurs des alarms */
