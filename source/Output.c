6cb40e315f0a55467bd425bc51a8fc2b95bf7b86
perecastor <archer.tb@hotmail.fr>
2014-03-26 12:52:08 +0100

    output tien compte de l activation des alarms

--- source/Output.c
+++ source/Output.c
@@ -2,6 +2,8 @@
 
 int port_g()
 {
+    int everything_is_safe = 1;
+
     unsigned char *portg,*ddrg;
     portg=(unsigned char *)0x02;
     /*Cast values into pointers*/
@@ -9,13 +11,35 @@ int port_g()
     *ddrg=0xff;
     /*Set DDRA to all outputs*/
 
-    if(get_alarm1() && get_alarm2() && get_alarm3() && get_alarm4() && get_alarm5() && get_alarm6() && get_alarm7() && get_alarm8() && get_alarm9()) /* everything is safe */
+    if(get_zones_active1())
     {
-        *portg = 0x00;
+        if(!(get_alarm1()&&get_alarm2()&&get_alarm3()))
+        {
+            everything_is_safe = 0;
+        }
+    }
+    if(get_zones_active2())
+    {
+        if(!(get_alarm4()&&get_alarm5()&&get_alarm6()))
+        {
+            everything_is_safe = 0;
+        }
+    }
+    if(get_zones_active3())
+    {
+        if(!(get_alarm7()&&get_alarm8()&&get_alarm9()))
+        {
+            everything_is_safe = 0;
+        }
+    }
+
+    if(everything_is_safe) /* everything is safe */
+    {
+        *portg = 0x00; /* no light */
     }
     else
     {
-        *portg = 0x01;
+        *portg = 0x01; /* light */
     }
 
     return 0;
