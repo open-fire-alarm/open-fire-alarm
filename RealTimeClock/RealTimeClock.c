a214d8f54458e2eb10b5a6a965d73a8af82b61d9
perecastor <archer.tb@hotmail.fr>
2014-04-02 12:18:16 +0200

    modification de la RealTimeClock pour la rendre utilisable pour open-fire-alarm

--- RealTimeClock/RealTimeClock.c
+++ RealTimeClock/RealTimeClock.c
@@ -4,50 +4,30 @@
 
 void timer(void); /*Function Prototype for ISR*/
 
-int hours,mins,secs,ticks,times; /* GLOBAL VARIABLES FOR RETURNING VALUES*/
+int hours,mins,secs,ticks; /* GLOBAL VARIABLES FOR RETURNING VALUES*/
 
-unsigned char *padr,*paddr,*tflg2,*pactl,*tmsk2;
+
 void main()
 {
-	padr=(unsigned char*)0x0;
-	paddr=(unsigned char*)0x1;
+    unsigned char *pactl,*tmsk2;
+
 	tmsk2=(unsigned char*)0x24;
-	tflg2=(unsigned char*)0x25;
 	pactl=(unsigned char*)0x26;
 
-	*paddr=0xff; /*Port A all outputs*/
 	*pactl=0x03; /*Set prescaler to maximum*/
 	*tmsk2=0x40; /*Enable RTI interrupt*/
-
-	for(;;)
-	{
-		if (ticks==0 && times==0) /*print out once a second*/
-		{
-			printf("%2i:%2i:%2i\r",hours,mins,secs);
-			times=1;
-		}
-		if (ticks==1)
-		{
-			times=0;
-		}
-	}
 }
 
 @interrupt void timer(void)
 {
+    unsigned char *tflg2;
+    tflg2=(unsigned char*)0x25;
+
 	ticks++;
 	if (ticks==30)
 	{
 		ticks=0;
 		secs++;
-		if (*padr==0)
-		{
-			*padr=0xff;
-		}
-		else
-		{
-			*padr=0x0; /*Flash lights */
-		}
 	}
 	if (secs==60)
 	{
