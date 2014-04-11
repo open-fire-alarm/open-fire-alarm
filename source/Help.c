fa1ece034b2eecc23b02c3df5e9a88015eabd2d2
perecastor <archer.tb@hotmail.fr>
2014-04-11 18:07:05 +0200

    ajout d un vrai help

--- source/Help.c
+++ source/Help.c
@@ -2,6 +2,8 @@
 
 int print_help()
 {
-    printf("help\n\r");
+    printf("Alarms are safe when alarm1 = 1 and on fire when alarm1 = 0 for example.\n\r");
+    printf("Zones are activated when zone1 = 1 and deactivated when zone1 = 0 for example.\n\r");
+    printf("In port a you have the eight alarms, the last alarm is on port e and the light on port g.\n\r");
     return 0;
 }
