94a3c92a4c5e7760f1b059bdb3c2522c0a6f1c1d
perecastor <archer.tb@hotmail.fr>
2014-04-11 20:28:04 +0200

    commentaire de Mylog.c

--- source/Mylog.c
+++ source/Mylog.c
@@ -5,7 +5,7 @@
 
 /* Mylog.c */
 
-char mylog[2000];
+char mylog[2000]; /* the log */
 
 int init_mylog()
 {
@@ -14,26 +14,26 @@ int init_mylog()
     return 0;
 }
 
-int add_line_to_mylog(char *new_line)
+int add_line_to_mylog(char *new_line) /* add the time and the string in parameter in the log */
 {
-    char new_line_final[1000];
+    char new_line_final[1000]; /* the goal is to create the new line */
     char string_time[1000];
     new_line_final[0]='\0';
     string_time[0]='\0';
-    return_time(string_time);
+    return_time(string_time); /* put the current time on the string */
     strcpy(new_line_final,string_time);
     strcat(new_line_final,"-> ");
     strcat(new_line_final,new_line);
     strcat(new_line_final,"\n\r");
     strcat(new_line_final,"\n\r");
 
-    strcat(mylog,new_line_final);
+    strcat(mylog,new_line_final); /* add the new line created to the log */
     return 0;
 }
 
-int update_mylog_zones_active()
+int update_mylog_zones_active() /* add a line to the log with the time about which zones are active */
 {
-    char new_line[1000];
+    char new_line[1000]; /* the goal is to create the new line */
     new_line[0]='\0';
     if(get_zones_active1() && get_zones_active2() && get_zones_active3())
     {
@@ -51,14 +51,14 @@ int update_mylog_zones_active()
     {
         strcat(new_line,"zone 3 inactive, ");
     }
-    add_line_to_mylog(new_line);
+    add_line_to_mylog(new_line); /* this function add the time and the string in parameter in the log */
 
     return 0;
 }
 
-int update_mylog_zones_on_fire()
+int update_mylog_zones_on_fire() /* add a line to the log with the time about which zones are on fire */
 {
-    char new_line[1000];
+    char new_line[1000]; /* the goal is to create the new line */
     new_line[0]='\0';
     if(get_zone1() && get_zone2() && get_zone3())
     {
@@ -76,13 +76,13 @@ int update_mylog_zones_on_fire()
     {
         strcat(new_line,"zone 3 on fire, ");
     }
-    add_line_to_mylog(new_line);
+    add_line_to_mylog(new_line); /* this function add the time and the string in parameter in the log */
 
     return 0;
 }
-int update_mylog_alarms_on_fire()
+int update_mylog_alarms_on_fire() /* add a line to the log with the time about which alarms are on fire */
 {
-    char new_line[1000];
+    char new_line[1000]; /* the goal is to create the new line */
     new_line[0]='\0';
     if(get_alarm1() && get_alarm2() && get_alarm3() && get_alarm4() && get_alarm5() && get_alarm6() && get_alarm7() && get_alarm8() && get_alarm9())
     {
@@ -125,14 +125,14 @@ int update_mylog_alarms_on_fire()
         strcat(new_line,"alarm9 fire, ");
     }
 
-    add_line_to_mylog(new_line);
+    add_line_to_mylog(new_line); /* this function add the time and the string in parameter in the log */
 
     return 0;
 }
 
-int print_mylog()
+int print_mylog() /* print the log */
 {
-    if(mylog[0]=='\0')
+    if(mylog[0]=='\0') /* print "no log for the moment" if no log */
     {
         printf("no log for the moment\n\r");
     }
